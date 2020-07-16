9bf812717caec895743eb2cfb7541fa670d8fc00
wangyh-quuo <2642369844@qq.com>
2020-07-16 14:54:02 +0800

    update: 单链表的操作

--- 数据结构/LinkList.js
+++ 数据结构/LinkList.js
@@ -1,37 +1,104 @@
 /**
- * 链表
+ * 链表 头结点head不存数据
  * 头插法 尾插法
  */
 class LinkList {
   constructor() {
     this.next = null;
-    this.length = 0;
   }
-  // 最后位置插入
+  // 尾插
   append(element) {
     let node = new Node(element);
-    let linkNode = this;
-    if (!linkNode.next) {
-      linkNode.next = node;
-    }else {
-      while (linkNode.next) {
-        linkNode = linkNode.next;
+    let head = this;
+    if (!head.next) {
+      head.next = node;
+    } else {
+      while (head.next) {
+        head = head.next;
       }
-      linkNode.next = node;
+      head.next = node;
     }
-    this.length++;
+  }
+  // 头插
+  headAppend(element) {
+    let node = new Node(element);
+    let head = this;
+    if (!head.next) {
+      head.next = node;
+    } else {
+      let p = head.next;
+      head.next = node;
+      node.next = p;
+    }
   }
   // 指定位置插入
-  insert(position, element) {}
+  insert(position, element) {
+    if (position >= 0 && position <= this.size()) {
+      let i = 0;
+      let p = this;
+      while (i <= position) {
+        p = p.next;
+        i++;
+      }
+      let node = new Node(element);
+      node.next = p.next;
+      p.next = node;
+    } else {
+      throw 'position is not exist';
+    }
+  }
   // 删除
-  remove(element) {}
+  remove(element) {
+    let pre = this;
+    let p = this.next;
+    while (p) {
+      if (p.element === element) {
+        pre.next = p.next;
+      }
+      pre = p;
+      p = p.next;
+    }
+  }
   // 删除指定某个位置
-  removeAt(position) {}
+  removeAt(position) {
+    if (position >= 0 && position <= this.size()) {
+      let i = 0;
+      let p = this,
+        pre = this;
+      while (i <= position) {
+        pre = p;
+        p = p.next;
+        i++;
+      }
+      pre.next = p.next;
+    } else {
+      throw 'position is not exist';
+    }
+  }
   // 获取当前节点的位置
-  indexOf(element) {}
+  indexOf(element) {
+    let p = this.next;
+    let index = -1;
+    let isExist = false;
+    while (p) {
+      index++;
+      if (p.element === element) {
+        isExist = true;
+        break;
+      }
+      p = p.next;
+    }
+    return isExist ? index : -1;
+  }
   // 获取链表的长度
   size() {
-    return this.length;
+    let length = 0;
+    let linkNode = this;
+    while (linkNode.next) {
+      linkNode = linkNode.next;
+      ++length;
+    }
+    return length;
   }
   // 是否为空
   isEmpty() {}
@@ -56,7 +123,13 @@ class Node extends LinkList {
 let list = new LinkList();
 for (let i = 0; i < 5; i++) {
   list.append(i);
+  list.headAppend(i);
 }
-console.log(JSON.stringify(list));
-console.log(list.toString());
-console.log('list length:', list.size())
+console.log("创建的链表",list.toString());
+list.insert(0, 200);
+console.log("0索引插入200",list.toString());
+list.remove(4);
+console.log("删除值为4的节点",list.toString());
+list.removeAt(4);
+console.log("删除索引为4的节点",list.toString());
+console.log('4的位置', list.indexOf(4));
